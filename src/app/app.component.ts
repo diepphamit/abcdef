6966664c5a9ae4e368b3f5de9bc21cf643a03073
Kevin <kevin@Macmini044.enclave.com>
2020-10-19 14:26:25 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  AfterViewInit{
   title = 'DemoSeniorProject';
+
+  ngAfterViewInit() {
+    console.log("abcd");
+  }
 }
